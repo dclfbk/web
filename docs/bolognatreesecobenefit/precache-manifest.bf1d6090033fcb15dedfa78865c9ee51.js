3293604e50c1427f9b622f5c87d4ce41b0ef6382
Maurizio Napolitano <napo@fbk.eu>
2024-04-18 22:24:52 +0200

    Bologna EcoTrees Benefit

---
+++ docs/bolognatreesecobenefit/precache-manifest.bf1d6090033fcb15dedfa78865c9ee51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "943c1483cf6925efa7f02cfdc51387c5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "72ba96cc045245d137aa",
+    "url": "./static/css/2.5c30fb7f.chunk.css"
+  },
+  {
+    "revision": "a42e00645f0fdb5d5c3e",
+    "url": "./static/css/main.6f55ac1a.chunk.css"
+  },
+  {
+    "revision": "72ba96cc045245d137aa",
+    "url": "./static/js/2.a1506b96.chunk.js"
+  },
+  {
+    "revision": "112b6cb3cc9122f16ecf",
+    "url": "./static/js/3.32b4e358.chunk.js"
+  },
+  {
+    "revision": "a42e00645f0fdb5d5c3e",
+    "url": "./static/js/main.2dce3f18.chunk.js"
+  },
+  {
+    "revision": "80493137cb4a7376c38d",
+    "url": "./static/js/runtime~main.997ade86.js"
+  }
+]);
